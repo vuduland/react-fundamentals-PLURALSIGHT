b514d309388b23104cea3828c7341d8b1fcc8331
Victor <53414263+vuduland@users.noreply.github.com>
2020-04-20 23:09:35 -0500

    configured context provider in App.js to allow globally accessible config values to any component beneath App.js

--- components/App.js
+++ components/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import Home from './Home';
 import Speakers from './Speakers';
 
+export const ConfigContext = React.createContext();
+// All we need to create shared context for the entire application. Hooks come in to 'consume' this app context.
+
 const pageToShow = (pageName) => {
   if (pageName === 'Home') return <Home />;
   if (pageName === 'Speakers') return <Speakers />;
   return <div>Not Found</div>;
 };
 
+const configValue = {
+  showSpeakerSpeakingDays: true,
+};
+// Below, we have wrapped our App component in a context provider and provided the configuration value above to it.
+// Theoretically, this will allow us to use this value in any component below the app component. (Think the components list in DevTools)
 const App = ({ pageName }) => {
-  return <div>{pageToShow(pageName)}</div>;
+  return (
+    <ConfigContext.Provider value={configValue}>
+      <div>{pageToShow(pageName)}</div>;
+    </ConfigContext.Provider>
+  );
 };
 
 export default App;
